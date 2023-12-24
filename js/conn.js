20872e1a1e8ab41ee00051117341cb31dc7b6eed
cubelet3d <68728119+cubelet3d@users.noreply.github.com>
2023-12-24 15:07:34 +0200

    Initial upload

---
+++ js/conn.js
@@ -0,0 +1,61 @@
+const VIDYA_ABI = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"internalType":"address","name":"addr","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"tokens","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"approveAndCall","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"addr","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"_min","type":"uint256"},{"internalType":"uint256","name":"_max","type":"uint256"}],"name":"distributePresale","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_presaleAddr","type":"address"}],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]; 
+
+let VIDYA; 
+let Inventory; 
+
+window.addEventListener('load', async () => {
+    if(window.ethereum) {
+		init(); 
+    }
+}); 
+
+var web3, accounts, connected = false; 
+
+async function init() {
+    try {
+		
+		await ethereum.request({method: 'eth_requestAccounts'}); 
+		
+        await setup(); 
+            
+		// Subscribe to accounts change
+		window.ethereum.on("accountsChanged", (accounts) => {
+			setup(); 
+		}); 
+			
+		// Subscribe to network change 
+		window.ethereum.on('chainChanged', function(networkId) {
+			setup(); 
+		});
+		
+    }
+	
+    catch(e) {
+        console.error(e)
+    }
+}
+
+async function setup() {
+    try {
+        web3 = new Web3(window.ethereum); 
+        accounts = await web3.eth.getAccounts(); 
+		
+        let chainID = await web3.eth.getChainId(); 
+		
+		if(chainID == 5) {
+			console.log("Connected to Goerli testnet!"); 
+			VidyaAddress = "0xFFE93E63E94da7A44739b6aBFA25B81CEd979a6b"; 
+			inventoryContract = "0xfbCc08d711664Fe9514404e4d9597774Ae3A0a63"; 
+		}
+		
+		Inventory = new web3.eth.Contract(inventoryABI, inventoryContract); 
+		VIDYA = new web3.eth.Contract(VIDYA_ABI, VidyaAddress); 
+
+		connected = true
+    }
+    
+    catch(e) {
+        console.error(e)
+    }
+    
+}
---
+++ start.bat
@@ -0,0 +1,2 @@
+start cmd /k http-server
+start chrome http://127.0.0.1:8080/
